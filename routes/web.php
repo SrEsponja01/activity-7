6294d45af760b4515e7228d99b29974df49ff505
SrEsponja01 <119963070+SrEsponja01@users.noreply.github.com>
2026-03-16 13:17:22 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Course; // Importamos tu modelo
 
+// Ruta principal (La pantalla de bienvenida de Laravel)
 Route::get('/', function () {
     return view('welcome');
+});
+
+// ¡Nuestra ruta secreta para ver los datos!
+Route::get('/mis-cursos', function () {
+    // Esto buscará los 100 cursos y traerá también la información del grupo al que pertenecen
+    return Course::with('group')->get(); 
 });
